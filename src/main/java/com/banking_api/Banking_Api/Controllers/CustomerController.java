60a0a0774ae5c102c4b3b33e8ec6c7aeccb443c3
Gery <gerybunguri@hotmail.com>
2024-11-25 00:21:25 +0200

    3nd commit

--- src/main/java/com/banking_api/Banking_Api/Controllers/CustomerController.java
+++ src/main/java/com/banking_api/Banking_Api/Controllers/CustomerController.java
@@ -38,7 +38,7 @@ public class CustomerController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
         customerService.deleteCustomer(id);
-        System.out.println("Check");
+        System.out.println("Check again!");
         return ResponseEntity.ok("Customer with ID " + id + " has been deleted.");
     }
 
